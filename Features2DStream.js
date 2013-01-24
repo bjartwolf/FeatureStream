28b43b29d86fc8d0b23111f7d438480b185a479a
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2013-01-24 22:36:19 +0100

    removed som shared global state in module. not tested it on drone yet

--- Features2DStream.js
+++ Features2DStream.js
@@ -11,20 +11,13 @@ util.inherits(Features2DStream, Stream);
 var h = 180;
 var w = 320;
 var nrOfPixels = w*h;
-var corners = [];
-var border = 3;
-var gray_img = new jsfeat.matrix_t(w, h, jsfeat.U8_t | jsfeat.C1_t);
-
-for (var i = 0; i < nrOfPixels; ++i) {
-   corners[i] = new jsfeat.point2d_t(0,0,0,0);
-}
 
 var cornerDetection = function (rgbaImage) {
-    jsfeat.imgproc.grayscale(rgbaImage, gray_img.data);
-    var count = jsfeat.fast_corners.detect(gray_img, corners, border);
+    jsfeat.imgproc.grayscale(rgbaImage, this.gray_img.data);
+    var count = jsfeat.fast_corners.detect(gray_img, this.corners, this.border);
     var non_null_corners = [];
     for (var i = 0; i < count; i++) {
-        non_null_corners.push(corners[i]);
+        non_null_corners.push(this.corners[i]);
     }
     return non_null_corners;
 }
@@ -33,6 +26,13 @@ function Features2DStream() {
    Stream.call(this);
    this.writable = true;
    this.readable = true;
+   this.corners = [];
+   this.border = 3;
+   this.gray_img = new jsfeat.matrix_t(w, h, jsfeat.U8_t | jsfeat.C1_t);
+
+   for (var i = 0; i < nrOfPixels; ++i) {
+      this.corners[i] = new jsfeat.point2d_t(0,0,0,0);
+   }
 }
 
 Features2DStream.prototype.write = function (rgbaImage) {
